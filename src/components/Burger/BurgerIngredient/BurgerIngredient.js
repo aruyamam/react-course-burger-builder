10eee166c91919bf87b696c26917f7ccd644e807
aruyamam <aruyamam@gmail.com>
2018-05-27 17:34:28 +0900

    Section 8 Outputting Burger Ingredients Dynamically

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -34,7 +34,7 @@ class BurgerIngredient extends Component {
         break;
 
       case ('bacon'):
-        ingredient = <div className={classes.Becon}></div>;
+        ingredient = <div className={classes.Bacon}></div>;
         break;
 
       default:
