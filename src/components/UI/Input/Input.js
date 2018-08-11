9a0287b30d9ac1ee9a3f41c0e58af71304883ce8
aruyamam <aruyamam@gmail.com>
2018-08-11 21:41:38 +0900

    Handling User Input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -9,20 +9,23 @@ const input = (props) => {
       case ('input'):
          inputElement = <input
             className={classes.InputElement} {...props.elementConfig}
-            value={props.value} />
+            value={props.value}
+            onChange={props.changed}/>
          break;
 
       case ('textarea'):
          inputElement = <textarea
             className={classes.InputElement} {...props.elementConfig}
-            value={props.value} />
+            value={props.value}
+            onChange={props.changed}/>
          break;
 
       case ('select'):
          inputElement = (
             <select
                className={classes.InputElement}
-               value={props.value}>
+               value={props.value}
+               onChange={props.changed}>
                {props.elementConfig.options.map((option) => (
                   <option key={option.value} vlaue={option.value}>
                      {option.displayValue}
@@ -34,8 +37,10 @@ const input = (props) => {
       
       default:
          inputElement = <input
-            className={classes.InputElement} {...props.elementConfig}
-            value={props.value} />
+            className={classes.InputElement}
+            {...props.elementConfig}
+            value={props.value} 
+            onChange={props.changed}/>
    }
 
    return (
