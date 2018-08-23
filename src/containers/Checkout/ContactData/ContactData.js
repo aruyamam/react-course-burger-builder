cb4d88d35239a6b018d6d36b8cee480812f424ef
aruyamam <aruyamam@gmail.com>
2018-08-23 21:39:45 +0900

    Adding Validation Feedback

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -111,10 +111,10 @@ class ContactData extends Component {
    };
 
    checkValidity(value, rules) {
-         let isValid = true;
+      let isValid = true;
 
       if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
+         isValid = value.trim() !== '' && isValid;
       }
       if (rules.minLnegth) {
          isValid = value.length >= rules.minLnegth && isValid;
@@ -159,6 +159,8 @@ class ContactData extends Component {
                   elementType={formElement.config.elementType}
                   elementConfig={formElement.config.elementConfig}
                   value={formElement.config.value}
+                  invalid={!formElement.config.valid}
+                  shouldValidate={formElement.config.validation}
                   changed={(event) =>
                      this.inputChangedHandler(event, formElement.id)
                   }
