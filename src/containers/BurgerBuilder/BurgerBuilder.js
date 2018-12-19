30ef0154d3ccc7e68296ad2747e8d0d324e712a7
aruyamam <aruyamam@gmail.com>
2018-12-19 10:55:34 +0900

    Testing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,7 +24,7 @@ const INGREDIENT_PRICES = {
    bacon: 0.7
 };
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
    state = {
       purchasable: false
    };
