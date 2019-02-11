30e893a80bddecd683c29f92743ef36b23b950fd
aruyamam <aruyamam@gmail.com>
2019-02-11 12:56:38 +0900

    Setting up the Project

--- src/App.js
+++ src/App.js
@@ -1,71 +1,10 @@
-import React, { Component, Fragment } from 'react';
-import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
-
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Logout from './containers/Auth/Logout/Logout';
-import { authCheckState } from './store/actions';
-
-const asyncCheckout = asyncComponent(() => {
-   return import('./containers/Checkout/Checkout');
-});
-const asyncOrders = asyncComponent(() => {
-   return import('./containers/Orders/Orders');
-});
-const asyncAuth = asyncComponent(() => {
-   return import('./containers/Auth/Auth');
-});
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
-   componentDidMount() {
-      this.props.onTryAutoSingup();
-   }
-
    render() {
-      const { isAuthenticated } = this.props;
-
-      let routes = (
-         <Switch>
-            <Route path="/auth" component={asyncAuth} />
-            <Route path="/" exact component={BurgerBuilder} />
-            <Redirect to="/" />
-         </Switch>
-      );
-
-      if (isAuthenticated) {
-         routes = (
-            <Switch>
-               <Route path="/checkout" component={asyncCheckout} />
-               <Route path="/orders" component={asyncOrders} />
-               <Route path="/logout" component={Logout} />
-               <Route path="/auth" component={asyncAuth} />
-               <Route path="/" exact component={BurgerBuilder} />
-               <Redirect to="/" />
-            </Switch>
-         );
-      }
-
-      return (
-         <div>
-            <Layout>{routes}</Layout>
-         </div>
-      );
+      return <div />;
    }
 }
 
-const mapStateToProps = state => ({
-   isAuthenticated: state.auth.token !== null
-});
-
-const mapDispatchToProps = dispatch => ({
-   onTryAutoSingup: () => dispatch(authCheckState())
-});
-
-export default withRouter(
-   connect(
-      mapStateToProps,
-      mapDispatchToProps
-   )(App)
-);
+export default App;
--- src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-   baseURL: 'https://react-my-burger-96345.firebaseio.com/'
-});
-
-export default instance;
