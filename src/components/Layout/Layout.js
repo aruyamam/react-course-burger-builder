53e39b3838c326247edd04e3e46d87e7d04d3868
aruyamam <aruyamam@gmail.com>
2019-02-11 14:01:40 +0900

    Creating a Layout Component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+const layout = ({ children }) => (
+   <Fragment>
+      <div>Toolbar, SideDrawer, Backdrop</div>
+      <main>{children}</main>
+   </Fragment>
+);
+
+export default layout;
