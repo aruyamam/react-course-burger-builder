b658d1c79fe1ef2900e80230b8dd967e07402075
aruyamam <aruyamam@gmail.com>
2019-02-12 12:04:18 +0900

    Adding the Buerger Builder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,17 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import classes from './Layout.module.css';
 
 const layout = ({ children }) => (
    <Fragment>
+      {console.log(children)}
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>{children}</main>
+      <main className={classes.Content}>{children}</main>
    </Fragment>
 );
+
+layout.propTypes = {
+   children: PropTypes.element.isRequired,
+};
 
 export default layout;
