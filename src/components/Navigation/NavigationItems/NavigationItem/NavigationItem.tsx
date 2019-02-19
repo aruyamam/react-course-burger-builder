c936df33677561cffd7c366a7957e2da19798b56
aruyamam <aruyamam@gmail.com>
2019-02-19 13:11:11 +0900

    Adding a Navigation Items

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './NavigationItem.module.css';
+
+interface Props {
+   active?: boolean;
+   children: React.ReactNode;
+   link: string;
+}
+
+const NavigationItem: React.FC<Props> = ({ active, children, link }) => (
+   <li className={classes.NavigationItem}>
+      {console.log(active)}
+      <a href={link} className={active ? classes.active : ''}>
+         {children}
+      </a>
+   </li>
+);
+
+export default NavigationItem;
