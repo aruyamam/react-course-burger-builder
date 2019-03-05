55acea8b2918df691b97658aa4b1f8b05b6caa0c
aruyamam <aruyamam@gmail.com>
2019-03-05 13:45:02 +0900

    Passing Ingredients via Query Params

--- src/containers/Checkout/Checkout.tsx
+++ src/containers/Checkout/Checkout.tsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import { IIngredients } from '../BurgerBuilder/BurgerBuilderTypes';
 
-class Checkout extends Component {
-   state = {
+class Checkout extends Component<RouteComponentProps> {
+   public readonly state = {
       ingredients: {
-         salad: 1,
-         meat: 1,
          cheese: 1,
          bacon: 2,
+         meat: 1,
+         salad: 1,
       },
+   };
+
+   componentDidMount() {
+      const { location } = this.props;
+      const query: URLSearchParams = new URLSearchParams(location.search);
+      const ingredients: IIngredients = {
+         cheese: 0,
+         bacon: 0,
+         meat: 0,
+         salad: 0,
+      };
+      for (const param of query.entries()) {
+         ingredients[param[0]] = +param[1];
+      }
+
+      this.setState({ ingredients });
+   }
+
+   public checkoutCancelHandler = () => {
+      const { history } = this.props;
+      history.goBack();
+   };
+
+   public checkoutContinueHandler = () => {
+      const { history } = this.props;
+      history.replace('/checkout/contact-data');
    };
 
    public render() {
@@ -16,7 +44,11 @@ class Checkout extends Component {
 
       return (
          <div>
-            <CheckoutSummary ingredients={ingredients} />
+            <CheckoutSummary
+               checkoutCancelled={this.checkoutCancelHandler}
+               checkoutContinued={this.checkoutContinueHandler}
+               ingredients={ingredients}
+            />
          </div>
       );
    }
